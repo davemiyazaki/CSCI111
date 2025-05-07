69fc6320b80bd2da5537ccca964c6d5ed55cf0d3
David Tkachenko <denixden2818@gmail.com>
2025-05-07 15:05:22 -0400

    Problem 2 completed.

--- Lab12/problem2.cpp
+++ Lab12/problem2.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <string>
+
 
 int main(){
-
+    const int ARRAY_SIZE = 3;
+    std::string nameArray[ARRAY_SIZE] = {"David", "Alice", "Bob"};
+    int numberArray[ARRAY_SIZE] = {100, 90, 80};
+    for(int i = 0; i < ARRAY_SIZE; i++){
+        std::cout << nameArray[i] << "'s score is " << numberArray[i] << "." << std::endl;
+    }
     return 0;
 }
